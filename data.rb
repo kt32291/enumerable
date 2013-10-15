39e96df0abbaa6795e6c4e80e4258f65ae2da7c7
kt32291 <katieastrauskas@apples-MacBook-Pro-2.local>
2013-10-15 16:29:39 -0500

    Completed Challenges

--- data.rb
+++ data.rb
@@ -4,11 +4,6 @@ puts "\nLoading data for the previous TSL students.. "
 
 load 'seeds.rb'
 
-STUDENTS.each do |name|
-  puts "#{name}"
-end
-
-
 
 # CHALLENGE 1
 
@@ -20,18 +15,52 @@ end
 # github: vcabansag
 # blog: http://vincentcabansag.com
 # email: vince@starterleague.com
+puts "CHALLENGE 1______________________________"
 
+STUDENTS.each do |name|
+  if name["last_name"] == "Cabansag"
+    puts "name: #{name["first_name"]} #{name["last_name"]}"
+    puts "twitter: #{name["twitter"]}"
+    puts "github: #{name["github"]}"
+    puts "blog: #{name["blog"]}"
+    puts "email: #{name["email"]}"
+  end
+end
 
 # CHALLENGE 2
 
 # Gather all of the Twitter handles into an array and show them on separate lines
 
+puts "CHALLENGE 2_______________________________"
 
+STUDENTS.each do |name|
+  if name["twitter"] != nil
+    puts name["twitter"]
+  end
+end
 
 # CHALLENGE 3
 
 # Update your script so that you only show values of a person if they're present
 # We don't want to see "blog: " or "github: "
+
+puts "CHALLENGE 3_______________________________"
+
+STUDENTS.each do |name|
+    puts "name: #{name["first_name"]} #{name["last_name"]}"
+  if name["twitter"] != nil
+    puts "twitter: #{name["twitter"]}"
+  end
+  if name["github"] != nil
+    puts "github: #{name["github"]}"
+  end
+  if name["blog"] != nil
+    puts "blog: #{name["blog"]}"
+  end
+  if name["email"] != nil
+    puts "email: #{name["email"]}"
+  end
+  end
 
 
 
@@ -40,7 +69,16 @@ end
 # Refactor your code to use the .keys method to access the key value pairs
 # We don't need to explicity type every key. Play around with the .keys method.
 
+puts "CHALLENGE 4________________________________________"
 
+STUDENTS.each do |name|
+  name.keys.each do |key|
+    if name[key] != nil
+      print key + ": "
+      puts name[key]
+    end
+  end
+end
 
 # BONUS CHALLENGE 5
 
@@ -59,6 +97,39 @@ end
 # def show_student_details(student)
 
 # end
+
+puts "CHALLENGE 5_____________________________________"
+
+puts "Enter first name:"
+
+def show_student_details
+
+first = gets.chomp
+
+STUDENTS.each do |name|
+  if name["first_name"] == first
+    puts "name: #{name["first_name"]} #{name["last_name"]}"
+    if name["twitter"] != nil
+      puts "twitter: #{name["twitter"]}"
+    end
+    if name["github"] != nil
+      puts "github: #{name["github"]}"
+    end
+    if name["blog"] != nil
+      puts "blog: #{name["blog"]}"
+    end
+    if name["email"] != nil
+      puts "email: #{name["email"]}"
+    end
+    puts ""
+  end
+end
+
+end
+
+show_student_details
+
+
 
 
 
